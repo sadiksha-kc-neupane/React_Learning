6e84db0ae88a263855e8496e33b0abcb2ca5d112
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-06-18 19:40:00 +0545

    made few new components and rendered them inside App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
+function Title() {
+  return <h1>This is title</h1>;
+}
+
+function Description() {
+  return <h2>This is Description</h2>;
+}
+
 function App() {
-  return <button>hello</button>;
+  return (
+    <div>
+      <h1>This is Heading</h1>
+      <Title />
+      <Description />
+    </div>
+  );
 }
 
 export default App;
