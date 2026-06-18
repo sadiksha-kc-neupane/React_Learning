843a4c48a786d030a188ab5959c535aa09231a4a
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-06-18 21:20:27 +0545

    made some static variable with an data in it and acssed it using curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ function Description() {
   return <h2>This is Description</h2>;
 }
 
+let name = "diksha";
+let caste = "K.C";
+let score = 70;
+
 function App() {
   return (
     <>
       <h1>This is Heading</h1>
       <Title />
       <Description />
+      <h2>
+        hi my name is {name} {caste} I scored {score}{" "}
+      </h2>
     </>
   );
 }
