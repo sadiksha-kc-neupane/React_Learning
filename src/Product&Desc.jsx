e2b932b8a561c78533763b5d4341b853ac25a939
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-06-20 23:23:41 +0545

    created an variable with keys and value in it and passed it as an props

--- src/Product&Desc.jsx
+++ src/Product&Desc.jsx
@@ -2,13 +2,18 @@ import Product from "./product.jsx";
 import "./Product_Desc.css";
 
 function Product_Desc() {
-  let features = ["hitech", "fast", "durable"];
+  // let features = ["hitech", "fast", "durable"];
+  let features2 = {
+    a: "This is durable",
+    b: "This is fast",
+    c: "This has 1 year waranty",
+  };
   return (
     <div className="Product_Desc">
       <Product
         title={"laptop"}
         description={"This is Asus Zenbook  i5 model with 8/256 gb storage"}
-        features={features}
+        features2={features2.a}
       />
       <Product title={"TV"} description={"This is a Smart TV"} />
       <Product
--- src/product.jsx
+++ src/product.jsx
@@ -1,10 +1,10 @@
-function Product({ title, description, features }) {
-  console.log(features);
+function Product({ title, description, features2 }) {
+  // console.log(features);
   return (
     <div className="Product">
       <h1>{title}</h1>
       <h4>{description}</h4>
-      <p>{features}</p>
+      <p>{features2}</p>
     </div>
   );
 }
