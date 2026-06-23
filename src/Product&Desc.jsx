22a2bfd4b550791df0fb6c0fc077c9b49384ab71
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-06-23 14:25:59 +0545

    added styling to product_des component

--- src/Product&Desc.jsx
+++ src/Product&Desc.jsx
@@ -2,14 +2,18 @@ import Product from "./product.jsx";
 import "./Product_Desc.css";
 
 function Product_Desc() {
+  let styles = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(220px, 1fr))",
+    gap: "20px",
+    padding: "20px",
+    justifyItems: "center",
+  };
   return (
-    <div className="Product_Desc">
+    <div style={styles} className="Product_Desc">
       <Product id={1} title="NovaCore X7 Gaming Processor" />
-
       <Product id={2} title="TitanForce RTX 980 Graphics Card" />
-
       <Product id={3} title="HyperSpeed 32GB DDR5 RAM Kit" />
-
       <Product id={4} title="QuantumVault 2TB NVMe SSD" />
     </div>
   );
